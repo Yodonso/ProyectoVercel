6efc91fa65de0385c357ad39d33274af71e931f0
Yodonso <yodonso@gmail.com>
2024-06-05 20:07:40 -0500

    se agrega messages

--- src/app.js
+++ src/app.js
@@ -1,30 +1,33 @@
 import express from "express";
 import morgan from 'morgan';
-import {join, dirname} from 'path';
-import {fileURLToPath} from 'url';
-import {engine} from 'express-handlebars';
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
+import messagesRoutes from "./routes/messages.routes.js"
 import companyRoutes from "./routes/company.routes.js"
 
-const app = express()
-const __dirname= dirname(fileURLToPath(import.meta.url));
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set("port", process.env.PORT || 4000)
-app.set('views', join(__dirname, 'views'))
-app.engine('hbs', engine({
+app.set("port", process.env.PORT || 4000);
+app.set('views', join(__dirname, 'views'));
+app.engine('.hbs', engine({
     defaultLayout: 'main',
     layoutsDir: join(app.get('views'), 'layouts'),
-    partialsDir:join(app.get('views'), 'partials'),
+    partialsDir: join(app.get('views'), 'partials'),
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
 
 app.use(morgan('dev'));
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('index')
-    })
+});
+
+app.use(messagesRoutes);
 app.use(companyRoutes);
 
 app.use(express.static(join(__dirname, 'public')))
